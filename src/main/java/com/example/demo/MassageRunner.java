ac365b42178cafd4de13c0eeb5d724f0cadc4cdb
hp <hp@DESKTOP-F2UU0A9>
2021-03-19 19:08:46 +0530

    WH 8080 first time

---
+++ src/main/java/com/example/demo/MassageRunner.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class MassageRunner implements CommandLineRunner{
+
+	@Override
+	public void run(String... args) throws Exception {
+System.out.println("WELCOME TO SPRING BOOT");
+	}
+
+}
