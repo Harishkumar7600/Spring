54b9ef9d09c4b1afd3842314fb25ca32ddf9eb23
hp <hp@DESKTOP-F2UU0A9>
2021-03-19 19:47:53 +0530

    WH-100

--- src/main/java/com/example/demo/MassageRunner.java
+++ src/main/java/com/example/demo/MassageRunner.java
@@ -6,6 +6,7 @@ public class MassageRunner implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
+		int a=10;
 System.out.println("WELCOME TO SPRING BOOT");
 System.out.println("WELCOME TO Hari");
 
